c0692280d54400527e6aa47c16606a9ef22133b2
Tymon <tymeczek12@gmail.com>
2021-01-26 09:33:58 +0100

    scrape update

--- scrape.py
+++ scrape.py
@@ -3,37 +3,38 @@ import json
 import pandas as pd
 import numpy as np
 import time
+from requests.exceptions import ConnectionError
 
+# List of unavailable countries
 UNAVAILABLE = []
 
 # Export funtion => exports DataFrames to CSV files for further use in data analysis
-def export(dataframe):
+def export(dataframe, country):
     # Each CSV file is going to be named after it's country
-    name = dataframe['Country'].unique()[0].lower()
-    if ' ' in name:
-        name = dataframe['Country'].unique()[0].replace(' ', '_').lower() 
-    dataframe.to_csv('./data/{}.csv'.format(name))
+    dataframe.to_csv('./data/{}.csv'.format(country))
 
 # Funtion to scrape data and put it nicely in a Pandas DataFrame
 def scrape_data(country):
     # Sleep, becouse there is a limit on requests 
     time.sleep(3)
-    # Try/Except becouse requesting sometimes throws an error ( due to limits )
+    # Step I - get the API's url, format for different countries 
+    URL = 'https://api.covid19api.com/total/dayone/country/{}/status/confirmed'
+    # Some URLs are unavailable, so Try/Except
     try:
-        # Step I - get the API's url, format for different countries 
-        URL = 'https://api.covid19api.com/total/dayone/country/{}/status/confirmed'
         # Step II - get request to the url, response to JSON
         res = requests.get(URL.format(country)).json()
+    except ConnectionError as err:
+        print(err)
+        print(URL + ' is unavailable')
+    # Try/Except becouse requesting sometimes throws an error ( due to limits )
+    try:
         # Step III - List comprehension to get lists of 1. Cases, 2. Dates, 3. Current country
         CASES = [res[i]['Date'][:10] for i in range(len(res))]
         DATE = [res[i]['Cases'] for i in range(len(res))]
-        COUNTRY = [res[i]['Country'] for i in range(len(res))]
         # Step IV - Creating the DataFrame with "Date" and "Cases" columns 
-        df = pd.DataFrame(list(zip(DATE, CASES)), columns=['Date', 'Cases'])
-        # Adding "Country" column
-        df['Country'] = COUNTRY
+        df = pd.DataFrame(list(zip(CASES, DATE)), columns=['Date', 'Cases'])
         print('\n' + 10*'=' + f' {country} dataframe created ' + 10*'=')
-        export(df)
+        export(df, country)
         print('\n' + 10*'=' + f' {country} dataframe exported ' + 10*'=')
     except KeyError or IndexError as err:
         print(err)
@@ -51,6 +52,7 @@ def main():
     # Creating the DataFrames
     for country in COUNTRIES:
         scrape_data(country)
+    print(UNAVAILABLE)
 
 main()
 
